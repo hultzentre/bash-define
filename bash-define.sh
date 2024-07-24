a77b83c1aceb9f03846005755c6636784e2becca
hultzentre <hultzentre@gmail.com>
2024-07-24 14:42:03 -0700

    Version 1

---
+++ bash-define.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#If called with args, use the first one, if not prompt for a word to define
+if [ $# -eq 0 ] then
+    read -p "Define => " word
+else
+    word=$1
+fi
+
+#curl request for specific word
+response=$(curl -s "https://api.dictionaryapi.dev/api/v2/entries/en/$word")
+
+#Output first sentence of the first three definitions
+echo "$response" |
+    grep -o '"definitions":\[[^]]*' |
+    awk -F'"definition":' '{ for(i=2; i<=NF; i++) print $i }' |
+    sed 's/^\["//' |
+    sed 's/",$//' |
+    sed 's/\([^".]*\)\..*/\1"/' |
+    sed 's/,"synonyms":.*//' |
+    head -n 3
